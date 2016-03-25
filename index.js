f378faa6cdc4d1587c031c5264f21c8ae9bbd923
Xiphe <github@xiphe.net>
2016-03-25 01:13:45 +0100

    feat(conventional-changelog-custom): extend conventional-changelog-angular

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 
+const deepAssign = require('deep-assign');
+const angularChangelogPromise = require('conventional-changelog-angular');
 const lib = require('./lib');
 
-module.exports = lib.getConfigFromRcFile()
-  .then((config) => ({ writerOpts: { transform: lib.getTransformFn(config) } }));
+module.exports = Promise.all([
+  lib.getConfigFromRcFile(),
+  angularChangelogPromise,
+]).then((config, conventionalChangelogAngular) => deepAssign(
+  {},
+  conventionalChangelogAngular,
+  { writerOpts: { transform: lib.getTransformFn(config) } }
+));
