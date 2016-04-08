58154f73cd057d014e1bb30b53ab71f008ecb7e8
Xiphe <github@xiphe.net>
2016-04-08 12:47:57 +0200

    chore(coveralls): add reporter

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ const gulp = require('gulp');
 const GulpToolboxRegistry = require('gulp-toolbox-registry');
 const testNodeJasmine = require('gulp-toolbox-test-node-jasmine');
 const pipeCoverageIstanbul = require('gulp-toolbox-pipe-coverage-istanbul');
+const reporterCoverage = require('gulp-toolbox-reporter-coveralls');
 
 gulp.registry(new GulpToolboxRegistry({
   toolboxes: [
     testNodeJasmine,
+    reporterCoverage,
   ],
   pipes: [
     pipeCoverageIstanbul,
